48377597a39966c2839de5ed72c418ae4b4de88d
Weirdo914 <44896455+Weirdo914@users.noreply.github.com>
2019-05-30 16:56:51 +0530

    Update donate.py

--- donate/donate.py
+++ donate/donate.py
@@ -15,7 +15,7 @@ class donate:
 
         #Your code will go here
         embed = discord.Embed(colour=0x00ff00)
-        embed.add_field(name=":point_right:Donate money:point_left:", value=<a href="https://donatebot.io/checkout/567325025649033236?buyer=425991701681930260">"Support"</a>)
+        embed.add_field(name=":point_right:Donate money:point_left:", value="https://bit.ly/2EJK7sV")
         embed.title = "Help support CRZA Esports"
         embed.set_footer(text="Bot by CRZA",  icon_url=icon)
         await self.bot.say(embed=embed)
