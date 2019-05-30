d3cfc837d67a0201975a0300028cca015f02f347
Weirdo914 <44896455+Weirdo914@users.noreply.github.com>
2019-05-30 16:01:03 +0530

    Update donate.py

--- donate/donate.py
+++ donate/donate.py
@@ -12,10 +12,10 @@ class donate:
         """This does stuff!"""
 
         #Your code will go here
-        embed = discord.Embed(title=Help support CRZA Esports, description = Support us, color = 0x00ff00)
-        embed.set_author(name=CRZA)
-        embed.add_field(name=: point_right:Donate money: point_left:, value = a href = "https://donatebot.io/checkout/567325025649033236?buyer=425991701681930260" Support < / a >, inline = True)
-        embed.set_footer(text=Bot by CRZA, icon_url = https: // cdn.discordapp.com / attachments / 569495909294145536 / 583588075498635280 / images_63_1.jpeg)
-        await self.bot.say(embed=embed)
+        embed = discord.Embed(colour=0x00ff00)
+        embed.add_field(name=:point_right:Donate money:point_left:, value=a href="https://donatebot.io/checkout/567325025649033236?buyer=425991701681930260" Support</a>, inline=True)
+        embed.title = "Help support CRZA Esports"
+        embed.set_footer(text=Bot by CRZA,  icon_url=https://cdn.discordapp.com/attachments/569495909294145536/583588075498635280/images_63_1.jpeg)
+        await self.bot.say(content=mention, embed=embed)
 def setup(bot):
-    bot.add_cog(Mycog(bot))
+    bot.add_cog(donate(bot))
