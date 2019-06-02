687b8ba749a5389c221a65a36eb74d85b7cb6d1e
Weirdo914 <44896455+Weirdo914@users.noreply.github.com>
2019-06-02 23:49:50 +0530

    Update donate.py

--- donate/donate.py
+++ donate/donate.py
@@ -49,11 +49,13 @@ class donate:
     @commands.command()
     async def donate(self):
         """Donate message"""
+        settings = self.check_server_settings(author.server)
+        title = settings["Title"]
 
         #Embed Code
         embed = discord.Embed(colour=0x00ff00)
         embed.add_field(name=":point_right:Donate money:point_left:", value="https://bit.ly/2EJK7sV")
-        embed.title = ["Title"]
+        embed.title = title
         embed.set_footer(text=credit,  icon_url=icon)
         await self.bot.say(embed=embed)
         
